c0bfefffa33e1db624dc2cdbde30f4725810224b
cottongin <cottongin@users.noreply.github.com>
2015-08-22 22:18:08 -0600

    Update plugin.py: fix nowPlaying, add YouTube result
    Due to LastFM update, lots of API functions are broken. This fixes (more of a band-aid) nowPlaying by scraping user's profile page rather than using API.
    
    Adds a YouTube link to the end of the reply for the currently playing / last played track.  (Requires YouTube API key, see https://developers.google.com/youtube/registering_an_application)

--- plugin.py
+++ plugin.py
@@ -37,10 +37,16 @@ import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 import supybot.world as world
 import supybot.log as log
+import json
+import urllib
+from lxml import html
+import requests
 
 import urllib2
 from xml.dom import minidom
 from time import time
+from time import mktime
+from datetime import datetime
 
 from LastFMDB import *
 
@@ -72,6 +78,35 @@ class LastFMParser:
         except (IndexError, AttributeError):
             album = None
         return (user, isNowPlaying, artist, track, album, time)
+        
+class NewLastFMParser:
+
+    def parseRecentTracks(self, stream):
+        """
+        @return Tuple with track information of last track
+        """
+        # Implemented to replace broken API after LastFM site update.
+        # Unfortunately this method (scraping last.fm profile page)
+        #  does not include album.
+
+        tree = html.fromstring(stream.text)
+        user = tree.xpath('//*[@id="content"]/header/div[2]/div/div[2]/div[1]/h1/text()')
+        user = ''.join(user).strip()
+        track = tree.xpath('//*[@id="recent-tracks-section"]/table/tbody/tr[1]/td[3]/span/a/text()')
+        track = ''.join(track).strip()
+        artist = tree.xpath('//*[@id="recent-tracks-section"]/table/tbody/tr[1]/td[3]/span/span[2]/a/text()')
+        artist = ''.join(artist).strip()
+        timestamp = tree.xpath('//*[@id="recent-tracks-section"]/table/tbody/tr[1]/td[4]/span/text()')
+        timestamp = ''.join(timestamp).strip()
+
+        if timestamp == "Scrobbling now":
+            isNowPlaying = True
+            time = timestamp
+        else:
+            time = timestamp
+            isNowPlaying = False
+
+        return (user, isNowPlaying, artist, track, time)
 
 class LastFM(callbacks.Plugin):
     # 1.0 API (deprecated)
@@ -92,6 +127,56 @@ class LastFM(callbacks.Plugin):
             world.flushers.remove(self.db.flush)
         self.db.close()
         self.__parent.die()
+        
+    def _yt(self, query):
+        """
+        @return link based on now playing results
+        """
+
+        url = "https://www.googleapis.com/youtube/v3/search"
+        key = "[put API key here (see https://developers.google.com/youtube/registering_an_application)]"
+        noresults = "No YouTube results found."
+
+        opts = {"q": query,
+                "part": "snippet",
+                "maxResults": 1,
+                "order": "relevance",
+                "key": key,
+                "safeSearch": "none"}
+
+        search_url = "%s?%s" % (url, urllib.urlencode(opts))    
+        result = False
+        
+        try:
+            response = utils.web.getUrl(search_url).decode("utf8")
+            data = json.loads(response)
+            
+            try:
+                items = data["items"]
+                
+                if items:
+                    video = items[0]
+                    snippet = video["snippet"]
+                    vid = video["id"]["videoId"]
+                    title = snippet["title"]
+                    result = True                    
+                    link = "https://youtu.be/%s" % (vid)
+                else:
+                    self.log.error("LastFM._yt: [ERR] unexpected API response")
+            
+            except IndexError, e:
+                self.log.error("LastFM._yt: [ERR] unexpected API response (%s)" % str(e))
+
+        except Exception, err:
+            self.log.error("LastFM._yt: [ERR] unexpected API response (%s)" % str(err))
+        
+        if result:
+            self.log.info("LastFM._yt: [URL] %s" % link)
+            return link
+        else:
+            #irc.sendMsg(ircmsgs.privmsg(message_destination, no_results_message))
+            self.log.info("LastFM._yt: [URL] %s" % no_results_message)
+            return noresults
 
     def lastfm(self, irc, msg, args, method, optionalId):
         """<method> [<id>]
@@ -134,26 +219,52 @@ class LastFM(callbacks.Plugin):
         or specify <id> to switch for one call.
         """
 
-        id = (optionalId or self.db.getId(msg.nick) or msg.nick)
+        # check to see if optionalId has been registered with the database
+        if optionalId:
+            try:
+                uid = self.db.getId(optionalId)
+                if not uid:
+                    uid = optionalId
+                    self.log.info("LastFM.nowPlaying: [INFO] optionalId not converted, using: %s" % uid)
+                else:
+                    self.log.info("LastFM.nowPlaying: [INFO] optionalId converted to LastFM id: %s" % uid)
+            except:
+                uid = optionalId
+                self.log.info("LastFM.nowPlaying: [INFO] optionalId not converted, using: %s" % uid)
+        else:
+            uid = (self.db.getId(msg.nick) or msg.nick)
+            self.log.info("LastFM.nowPlaying: [INFO] optionalId not given, using: %s" % uid)
 
         # see http://www.lastfm.de/api/show/user.getrecenttracks
-        url = "%s&method=user.getrecenttracks&user=%s" % (self.APIURL_2_0, id)
+        # url = "%s&method=user.getrecenttracks&user=%s" % (self.APIURL_2_0, id)
+        url = "http://www.last.fm/user/%s" % (uid)
         try:
-            f = urllib2.urlopen(url)
-        except urllib2.HTTPError:
-            irc.error("Unknown ID (%s)" % id)
+            f = requests.get(url)
+        except requests.HTTPError:
+            irc.reply("LastFM: Unknown ID %s" % uid)
             return
 
-        parser = LastFMParser()
-        (user, isNowPlaying, artist, track, album, time) = parser.parseRecentTracks(f)
-        albumStr = "[" + album + "]" if album else ""
-        if isNowPlaying:
-            irc.reply(('%s is listening to "%s" by %s %s'
-                    % (user, track, artist, albumStr)).encode("utf8"))
+        if f.status_code == 404:
+            irc.reply("LastFM: Unknown ID %s" % uid)
         else:
-            irc.reply(('%s listened to "%s" by %s %s more than %s'
-                    % (user, track, artist, albumStr,
-                        self._formatTimeago(time))).encode("utf-8"))
+            parser = NewLastFMParser()
+            (user, isNowPlaying, artist, track, time) = parser.parseRecentTracks(f)
+            if not track:
+                irc.reply("No information returned, it's possible user " + uid + " hasn't played anything.")
+            else:
+                ytquery = track + " by " + artist
+                link = self._yt(ytquery)
+                if isNowPlaying is True:
+                    irc.reply(('%s is listening to "%s" by %s | %s'
+                            % (user, track, artist, link)).encode("utf8"))
+                else:
+                    if "ago" in time:
+                        irc.reply(('%s listened to "%s" by %s %s | %s'
+                                % (user, track, artist, time, link)).encode("utf-8"))
+                    else:
+                        irc.reply(('%s listened to "%s" by %s %s | %s'
+                                % (user, track, artist,
+                                    self._formatTimeago(time), link)).encode("utf-8"))
 
     np = wrap(nowPlaying, [optional("something")])
 
@@ -238,6 +349,27 @@ Country: %s; Tracks played: %s" % ((id,) + profile)).encode("utf8"))
                 return exceptMsg
 
     def _formatTimeago(self, unixtime):
+        if isinstance(unixtime, basestring):
+            year = datetime.now().year
+            # Due to last.fm site update, this has to be modified to expect a string at first
+            # and then converted, since the data is scraped (see NewLastFMParser).
+            
+            # check if string already contains year and insert it if it doesn't, if track was 
+            # played during the current year it will not include it.
+            if len(unixtime) <= 14:
+                splitstring = unixtime.rpartition(':')
+                # string returned from LastFM does not include leading zeroes in date
+                # for example jan 1 instead of jan 01
+                if splitstring[0][-2].isdigit() == True:
+                    unixtime = splitstring[0][:len(splitstring[0])-2] + "%i, " % year + \
+                        splitstring[0][-2:] + splitstring[1] + splitstring[2]
+                else:
+                    unixtime = splitstring[0][:len(splitstring[0])-1] + "%i, " % year + \
+                        splitstring[0][-1:] + splitstring[1] + splitstring[2]
+            
+            # convert string to timestamp
+            unixtime = mktime(datetime.strptime(unixtime, "%d %b %Y, %I:%M%p").timetuple())
+            
         t = int(time()-unixtime)
         if t/86400 > 0:
             return "%i days ago" % (t/86400)
